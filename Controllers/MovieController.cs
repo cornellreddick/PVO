329f6e86ac904a405df5470cda1f369cdabba162
Cornell Reddick <cornellreddick@outlook.com>
2019-11-22 09:36:54 -0500

    Movie application using ASP.NET MVC 5

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using PVO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PVO.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Ramdom
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+        }
+
+        
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PVO.Startup))]
+namespace PVO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
