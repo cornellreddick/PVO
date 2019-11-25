3af84e759fd648c22ebc38231bcc8ee2b3605cb1
Cornell Reddick <cornellreddick@outlook.com>
2019-11-25 09:13:42 -0500

    Updated the movie Controller with a GetMovies, and detail method.

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -13,53 +13,53 @@ namespace PVO.Controllers
         // GET: Movie/Ramdom
         public ActionResult Random()
         {
-            var movie = new Movie() { Name = "Shrek!" };
+            var movie = GetMovies();
 
-//            var movie = new List<Movie>
-//            {
-//                new Movie { Name = "Cornell"},
-//                new Movie { Name = "Tasha"},
-//                new Movie { Name = "Sam"},
-//                new Movie { Name = "Reese"},
-//
-//            };
+            return View(movie);
+        }
 
-            var viewModel = new RandomMovieViewModel
+        public ActionResult Details( int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+        };
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
             {
-                Movie = movie,
-
-                //Customers = customers
-
+                new Movie {Name = "Queen & Slim"},
+                new Movie {Name = "Midway"},
+                new Movie {Name = "Frozen II"}
             };
-
-            return View(viewModel);
         }
 
-        public ActionResult Edit(int Id)
-        {
-            return Content("Id=" + Id);
-        }
 
-        //Movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (pageIndex.HasValue)
-                pageIndex = 1;
-            
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-
-        //Attribute Route
-        [Route("movie/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseYear(int year, int month)
-        {
-
-            return Content(year + "/" + month);
-        }
+        //Example methods 
+//        public ActionResult Edit(int Id)
+//        {
+//            return Content("Id=" + Id);
+//        }
+//
+//        //Movies
+//        public ActionResult Index(int? pageIndex, string sortBy)
+//        {
+//            if (pageIndex.HasValue)
+//                pageIndex = 1;
+//            
+//
+//            if (String.IsNullOrWhiteSpace(sortBy))
+//                sortBy = "Name";
+//
+//            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+//        }
+//
+//        //Attribute Route
+//        [Route("movie/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+//        public ActionResult ByReleaseYear(int year, int month)
+//        {
+//
+//            return Content(year + "/" + month);
+//        }
 
                  
     }
