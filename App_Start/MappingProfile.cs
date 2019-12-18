f2ecd8887bb9275819f70236a0419a5e19c7aa57
Cornell Reddick <cornellreddick@outlook.com>
2019-12-18 15:16:51 -0500

    Updated MappingProfile, Customers & Movies Controllers

--- App_Start/MappingProfile.cs
+++ App_Start/MappingProfile.cs
@@ -12,8 +12,15 @@ namespace PVO.App_Start
     {
         public MappingProfile()
         {
+            // Domain to Dto
             Mapper.CreateMap<Customer, CustomerDto>();
             Mapper.CreateMap<CustomerDto, Customer>();
+
+            // Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(m => m.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(m => m.Id, opt => opt.Ignore());
         }
-    }
 }
--- Dtos/MovieDto.cs
+++ Dtos/MovieDto.cs
@@ -16,13 +16,13 @@ namespace PVO.Dtos
         [StringLength(255)]
         public string Name { get; set; }
 
-        public Genre Genre { get; set; }
-
+        [Required]
         public byte GenreId { get; set; }
         public DateTime ReleaseDate { get; set; }
 
         public DateTime DateAdded { get; set; }
 
+        [Range(1, 20)]
         public byte NumberInStock { get; set; }
     }
 }
