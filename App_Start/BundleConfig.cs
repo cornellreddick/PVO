ce1c4aa3928b7065b52deb64fa61cb30a36bd420
Cornell Reddick <cornellreddick@outlook.com>
2019-12-19 15:39:45 -0500

    Added bootbox to the customer view.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -20,9 +20,11 @@ namespace PVO
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js",
+                      "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+                                bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-luman.css",
                       "~/Content/site.css"));
         }
